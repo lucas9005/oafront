7aa2eb7cd78353d6719b73b7963639ea260de7cd
Lucas Aaron Ruggiero <lucas9005@gmail.com>
2017-04-28 19:39:13 -0300

    Images Optimization

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -29,25 +29,25 @@ export class GalleryComponent implements OnInit {
     this.images.push({src:'../../../assets/7.jpg', alt:'Alt Imagen 7'});
     this.images.push({src:'../../../assets/8.jpg', alt:'Alt Imagen 8'});
     this.images.push({src:'../../../assets/9.jpg', alt:'Alt Imagen 9'});
-    this.images.push({src:'../../../assets/10.jpg', alt:'Alt Imagen 10'});
-    this.images.push({src:'../../../assets/11.jpg', alt:'Alt Imagen 11'});
-    this.images.push({src:'../../../assets/12.jpg', alt:'Alt Imagen 12'});
-    this.images.push({src:'../../../assets/13.jpg', alt:'Alt Imagen 13'});
-    this.images.push({src:'../../../assets/14.jpg', alt:'Alt Imagen 14'});
-    this.images.push({src:'../../../assets/15.jpg', alt:'Alt Imagen 15'});
-    this.images.push({src:'../../../assets/16.jpg', alt:'Alt Imagen 16'});
-    this.images.push({src:'../../../assets/17.jpg', alt:'Alt Imagen 17'});
-    this.images.push({src:'../../../assets/18.jpg', alt:'Alt Imagen 18'});
-    this.images.push({src:'../../../assets/19.jpg', alt:'Alt Imagen 19'});
-    this.images.push({src:'../../../assets/20.jpg', alt:'Alt Imagen 20'});
-    this.images.push({src:'../../../assets/21.jpg', alt:'Alt Imagen 21'});
-    this.images.push({src:'../../../assets/22.jpg', alt:'Alt Imagen 22'});
-    this.images.push({src:'../../../assets/23.jpg', alt:'Alt Imagen 23'});
-    this.images.push({src:'../../../assets/24.jpg', alt:'Alt Imagen 24'});
-    this.images.push({src:'../../../assets/25.jpg', alt:'Alt Imagen 25'});
-    this.images.push({src:'../../../assets/26.jpg', alt:'Alt Imagen 26'});
-    this.images.push({src:'../../../assets/27.jpg', alt:'Alt Imagen 27'});
-    this.images.push({src:'../../../assets/28.jpg', alt:'Alt Imagen 28'});
+    this.images.push({src:'../../../assets/1.jpg', alt:'Alt Imagen 10'});
+    this.images.push({src:'../../../assets/2.jpg', alt:'Alt Imagen 11'});
+    this.images.push({src:'../../../assets/3.jpg', alt:'Alt Imagen 12'});
+    this.images.push({src:'../../../assets/4.jpg', alt:'Alt Imagen 13'});
+    this.images.push({src:'../../../assets/5.jpg', alt:'Alt Imagen 14'});
+    this.images.push({src:'../../../assets/6.jpg', alt:'Alt Imagen 15'});
+    this.images.push({src:'../../../assets/7.jpg', alt:'Alt Imagen 16'});
+    this.images.push({src:'../../../assets/8.jpg', alt:'Alt Imagen 17'});
+    this.images.push({src:'../../../assets/9.jpg', alt:'Alt Imagen 18'});
+    this.images.push({src:'../../../assets/1.jpg', alt:'Alt Imagen 19'});
+    this.images.push({src:'../../../assets/2.jpg', alt:'Alt Imagen 20'});
+    this.images.push({src:'../../../assets/3.jpg', alt:'Alt Imagen 21'});
+    this.images.push({src:'../../../assets/4.jpg', alt:'Alt Imagen 22'});
+    this.images.push({src:'../../../assets/5.jpg', alt:'Alt Imagen 23'});
+    this.images.push({src:'../../../assets/6.jpg', alt:'Alt Imagen 24'});
+    this.images.push({src:'../../../assets/7.jpg', alt:'Alt Imagen 25'});
+    this.images.push({src:'../../../assets/8.jpg', alt:'Alt Imagen 26'});
+    this.images.push({src:'../../../assets/9.jpg', alt:'Alt Imagen 27'});
+    this.images.push({src:'../../../assets/1.jpg', alt:'Alt Imagen 28'});
   }
 
   setOpenedStateTrue(openedSrc, openedAlt){
