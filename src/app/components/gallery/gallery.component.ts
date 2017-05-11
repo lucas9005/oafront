b9e02f53616403f7bba87adf85bff031489819fd
Lucas Aaron Ruggiero <lucas9005@gmail.com>
2017-05-11 15:06:28 -0300

    Added thumbnails to gallery images

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -7,59 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
   images: any[];
-  openedState: Boolean;
-  openedSrc: string;
-  openedAlt: string;
+  openedState: boolean;
+  openedId: string;
+  openedDescription: string;
 
   constructor() {
     this.images = [];
     this.openedState = false;
-    this.openedSrc = null;
-    this.openedAlt = null;
+    this.openedId = null;
+    this.openedDescription = null;
   }
 
   ngOnInit() {
     //Estos datos se van a consumir desde una API en un futuro
-    this.images.push({src:'../../../assets/1.jpg', alt:'Imagen 1'});
-    this.images.push({src:'../../../assets/2.jpg', alt:'Imagen 2'});
-    this.images.push({src:'../../../assets/3.jpg', alt:'Imagen 3'});
-    this.images.push({src:'../../../assets/4.jpg', alt:'Imagen 4'});
-    this.images.push({src:'../../../assets/5.jpg', alt:'Imagen 5'});
-    this.images.push({src:'../../../assets/6.jpg', alt:'Imagen 6'});
-    this.images.push({src:'../../../assets/7.jpg', alt:'Imagen 7'});
-    this.images.push({src:'../../../assets/8.jpg', alt:'Imagen 8'});
-    this.images.push({src:'../../../assets/9.jpg', alt:'Imagen 9'});
-    this.images.push({src:'../../../assets/10.jpg', alt:'Imagen 10'});
-    this.images.push({src:'../../../assets/11.jpg', alt:'Imagen 11'});
-    this.images.push({src:'../../../assets/12.jpg', alt:'Imagen 12'});
-    this.images.push({src:'../../../assets/13.jpg', alt:'Imagen 13'});
-    this.images.push({src:'../../../assets/14.jpg', alt:'Imagen 14'});
-    this.images.push({src:'../../../assets/15.jpg', alt:'Imagen 15'});
-    this.images.push({src:'../../../assets/16.jpg', alt:'Imagen 16'});
-    this.images.push({src:'../../../assets/17.jpg', alt:'Imagen 17'});
-    this.images.push({src:'../../../assets/18.jpg', alt:'Imagen 18'});
-    this.images.push({src:'../../../assets/19.jpg', alt:'Imagen 19'});
-    this.images.push({src:'../../../assets/20.jpg', alt:'Imagen 20'});
-    this.images.push({src:'../../../assets/21.jpg', alt:'Imagen 21'});
-    this.images.push({src:'../../../assets/22.jpg', alt:'Imagen 22'});
-    this.images.push({src:'../../../assets/23.jpg', alt:'Imagen 23'});
-    this.images.push({src:'../../../assets/24.jpg', alt:'Imagen 24'});
-    this.images.push({src:'../../../assets/25.jpg', alt:'Imagen 25'});
-    this.images.push({src:'../../../assets/26.jpg', alt:'Imagen 26'});
-    this.images.push({src:'../../../assets/27.jpg', alt:'Imagen 27'});
-    this.images.push({src:'../../../assets/28.jpg', alt:'Imagen 28'});
+    this.images.push({id:'1', description:'Imagen 1'});
+    this.images.push({id:'2', description:'Imagen 2'});
+    this.images.push({id:'3', description:'Imagen 3'});
+    this.images.push({id:'4', description:'Imagen 4'});
+    this.images.push({id:'5', description:'Imagen 5'});
+    this.images.push({id:'6', description:'Imagen 6'});
+    this.images.push({id:'7', description:'Imagen 7'});
+    this.images.push({id:'8', description:'Imagen 8'});
+    this.images.push({id:'9', description:'Imagen 9'});
+    this.images.push({id:'10', description:'Imagen 10'});
+    this.images.push({id:'11', description:'Imagen 11'});
+    this.images.push({id:'12', description:'Imagen 12'});
+    this.images.push({id:'13', description:'Imagen 13'});
+    this.images.push({id:'14', description:'Imagen 14'});
+    this.images.push({id:'15', description:'Imagen 15'});
+    this.images.push({id:'16', description:'Imagen 16'});
+    this.images.push({id:'17', description:'Imagen 17'});
+    this.images.push({id:'18', description:'Imagen 18'});
+    this.images.push({id:'19', description:'Imagen 19'});
+    this.images.push({id:'20', description:'Imagen 20'});
+    this.images.push({id:'21', description:'Imagen 21'});
+    this.images.push({id:'22', description:'Imagen 22'});
+    this.images.push({id:'23', description:'Imagen 23'});
+    this.images.push({id:'24', description:'Imagen 24'});
+    this.images.push({id:'25', description:'Imagen 25'});
+    this.images.push({id:'26', description:'Imagen 26'});
+    this.images.push({id:'27', description:'Imagen 27'});
+    this.images.push({id:'28', description:'Imagen 28'});
   }
 
-  setOpenedStateTrue(openedSrc, openedAlt){
+  setOpenedStateTrue(openedId, openedDescription){
     this.openedState = true;
-    this.openedSrc = openedSrc;
-    this.openedAlt = openedAlt;
+    this.openedId = openedId;
+    this.openedDescription = openedDescription;
   }
 
   setOpenedStateFalse(){
     this.openedState = false;
-    this.openedSrc = null;
-    this.openedAlt = null;
+    this.openedId = null;
+    this.openedDescription = null;
   }
 }
 
